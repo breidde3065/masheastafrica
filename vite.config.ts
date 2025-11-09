3e7c1107073b04b656d516ce4749a0cd90c727f4
breidde3065 <breidde3065@mailfence.com>
2025-11-09 13:51:43 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
-    
     return {
-        base:'/masheastafrica/',
+      base:'/masheastafrica',
       server: {
         port: 3000,
         host: '0.0.0.0',
